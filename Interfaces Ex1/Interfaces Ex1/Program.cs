87b182c8402cba8e6b6a098f812cc6ccde2c959b
William Magnusson <82813924+Magnusson132@users.noreply.github.com>
2025-10-16 19:17:57 +0200

    Add files via upload

---
+++ Interfaces Ex1/Interfaces Ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+interface Iswimmable
+{
+    void Swim();
+}
+
+class Fish : Iswimmable
+{
+    public string Name;
+
+    public Fish(string name)
+    {
+        Name = name;
+    }
+
+    public void Swim() => Console.WriteLine($"{Name} is swimming");
+}
+
+class Program
+{
+    public static void Main()
+    {
+        List<Iswimmable> swimmingObjects = new List<Iswimmable>();
+        swimmingObjects.Add((new Fish("Abborren")));
+        swimmingObjects.Add(new Fish("Gäddan"));
+
+        foreach (Iswimmable obj in swimmingObjects)
+        {
+            obj.Swim();
+        }
+    }
+}
