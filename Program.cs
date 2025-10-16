438c64b6a6b2c3fceab694c7368988d9a5b430d1
William Magnusson <82813924+Magnusson132@users.noreply.github.com>
2025-10-16 18:44:22 +0200

    Add files via upload
    Methods and fields ex 2

--- Program.cs
+++ Program.cs
@@ -1,67 +1,37 @@
-﻿/*using System;
-
-class Book
+﻿using System;
+             // USED WITH USER INPUT! :D 
+class Rectangle
 {
-    public string title;
-    public string author;
-    public int pages;
+    public double width;
+    public double height;
 
-    public static void Main()
+    public double Area()
     {
-        Book firstBook = new Book();
-        firstBook.title = "Harry Potter";
-        firstBook.author = "J.K Rowling";
-        firstBook.pages = 432;
-
-        Book secondBook = new Book();
-        secondBook.title = "Lord of The Rings";
-        secondBook.author = "J.J.R Tolkien";
-        secondBook.pages = 604;
-        
-        Console.WriteLine($"Title: {firstBook.title}, Author: {firstBook.author}, Pages: {firstBook.pages}");
-        Console.WriteLine($"Title: {secondBook.title}, Author: {secondBook.author}, Pages: {secondBook.pages}");
+        return width * height;
+        Console.WriteLine($"The area is: {Area}");
     }
-}*/
 
-using System;
-
-
-//  ---------- TEST AV CONOSTRUCTOR, EJ KLAR, ORKADE INTE OCH VAR INTE EN DEL
-// ------------ AV UPPGIFTEN!
-class Student
-{
-    public string name;
-    public int age;
-    public string favoriteSubject;
-
-    public Book(string title, int age, string favoriteSubject)
+    public double Perimeter()
     {
-        this.title = title;
-        this.age = age;
-        this.favoriteSubject = favoriteSubject;
+        return 2 * (width + height);
+        Console.WriteLine($"The perimiter is : {Perimeter()}");
+       
+    }
 
+    class Program
+    {
+        public static void Main()
+        {
+            Rectangle rect = new Rectangle();
+            
+            rect.width = double.Parse(Console.ReadLine());
+            rect.height = double.Parse(Console.ReadLine());
+
+            double area = rect.Area();
+            double perimeter = rect.Perimeter();
+            
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter: {perimeter}");
+        }
     }
 }
-
-/*public static void Main()
-{
-    Student firstStudent = new Student();
-    Student secondStudent = new Student();
-    Student thirdtudent = new Student();
-
-
-    firstStudent.name = "Hanna";
-    firstStudent.age = 22;
-    firstStudent.favoriteSubject = "Art";
-
-    secondStudent.name = "William";
-    secondStudent.age = 34;
-    secondStudent.favoriteSubject = "Music";
-
-    thirdtudent.name = "Noah";
-    thirdtudent.age = 5;
-    thirdtudent.favoriteSubject = "Games";
-
-    Console.WriteLine($"Name: {firstStudent.name}, age: {firstStudent.age}, Favorite: {firstStudent.favoriteSubject}");
-}
-}*/
