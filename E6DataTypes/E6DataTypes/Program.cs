1b5e0389a74aef18e0b298409cdef064f569f100
William Magnusson <texas.magnusson@gmail.com>
2025-10-16 22:36:09 +0200

    Exercise 6 - Data Types
    Exercise 6 - Data Types

---
+++ E6DataTypes/E6DataTypes/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+ --------------- WHOLE NUMBERS ----------
+int myAge = 99;
+
+int catsInterestInPeople = -10;
+
+int numberOfPets = 7;
+
+int playerLeveel = 77;
+
+int nrOfItemsInInventory = 16;
+*/
+// ------- FACTIONAL NUMBERS -------
+/*
+float worldCoordinate = 52.567f;
+
+float multiplier = 3.25f;
+
+float interestRate = .5f;
+
+double bacterialSiza = 24e-10d;
+
+double atomSize = 0.000000001d;
+*/
+//------------ TRUE OR FALSE FLAG -----------
+/*bool isPlayerdead = false;
+
+bool lovesProgramming = true;
+
+bool isDoorOpen = false;
+
+bool gameOver = true;
+
+Console.WriteLine(isDoorOpen);*/
+//---------------- SINGLE LETTERS --------
+
+/*
+char c = 'C';
+
+char a = 'A';
+
+char k = 'K';
+
+int result = c - k;
+Console.WriteLine(result);
+*/
+// ----------- TEXT -------------
+
+/*
+string name = "William";
+
+Console.WriteLine("Hello, World.\n This is a new line.");
+Console.WriteLine("If you want to print \" Qoutes \", escape them.");
+Console.WriteLine("Better but some \t\t Distance then.");
+
+string starWarsIntro = @"Star wars
+Episode IV:
+A New Hope";
+
+string response = $"Welcome, {name}!";
+
+string movie = $"Lets watch, {starWarsIntro}!";
+
+Console.WriteLine(response);
+Console.WriteLine(movie);
+*/
+// -----------GOAL------------
+
+string outputString = "Hello world";
+
+Console.WriteLine(outputString);
+
+float decimalValue = 0.3f;
+
+Console.WriteLine(decimalValue);
+
+bool isAwsome = true;
+
+Console.WriteLine(isAwsome);
