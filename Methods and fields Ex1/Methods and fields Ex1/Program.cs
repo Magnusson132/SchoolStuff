f5414a29e829a1cd1222e0fe6c6e910b370220cd
William Magnusson <82813924+Magnusson132@users.noreply.github.com>
2025-10-16 19:15:44 +0200

    Add files via upload
    Methods and fields Exercise1

---
+++ Methods and fields Ex1/Methods and fields Ex1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Lamp
+{
+    public bool isOn;
+    
+    //Method to turn it On
+    public void TurnOn()
+    {
+        isOn = true;
+        Console.WriteLine("Lamp is On!");
+    }
+
+    public void TurnOff()
+    {
+        isOn = false;
+        Console.WriteLine("Lamp is Off!");
+    }
+
+    public void Toggle()
+    {
+        isOn = !isOn;
+        if (isOn)
+            Console.WriteLine("Lamp is ON.");
+        else
+            Console.WriteLine("Lamp is OFF.");
+    }
+}
+
+class Program
+    {
+        static void Main()
+        {
+
+            Lamp myLamp = new Lamp();
+            
+            myLamp.TurnOn();
+            myLamp.Toggle();
+            myLamp.Toggle();
+            myLamp.TurnOff();
+            
+            Console.WriteLine($"Is the lamp on? {myLamp}");
+            
+            myLamp.Toggle();
+            
+            Console.WriteLine($"Is the lamp on? {myLamp}");
+            
+            myLamp.Toggle();
+            
+            Console.WriteLine($"Is the lamp on? {myLamp}");
+            
+            
+        }
+    }
